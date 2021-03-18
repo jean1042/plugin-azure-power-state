87003e78b71ac68f255510aeb9d533f46e4f024b
jean1042 <jean1042@naver.com>
2021-03-18 15:57:16 +0900

    Add VmScaleSetManager, Subscription Manager

--- src/spaceone/inventory/manager/__init__.py
+++ src/spaceone/inventory/manager/__init__.py
@@ -1,5 +1,7 @@
 from spaceone.inventory.libs.manager import AzureManager
 from spaceone.inventory.manager.virtual_machine_manager import VirtualMachineManager
+from spaceone.inventory.manager.virtual_machine_scale_set_manager import VmScaleSetManager
+from spaceone.inventory.manager.subscription_manager import SubscriptionManager
 
 
 
