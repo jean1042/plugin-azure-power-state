0685bb0f3d9434eee6418b3dbfcd288b2b32b949
jean1042 <jean1042@naver.com>
2021-03-18 15:54:38 +0900

    add vmscaleset connector, subscription connector

--- src/spaceone/inventory/connector/__init__.py
+++ src/spaceone/inventory/connector/__init__.py
@@ -1,3 +1,4 @@
 from spaceone.inventory.libs.connector import AzureConnector
 from spaceone.inventory.connector.virtual_machine import VirtualMachineConnector
-
+from spaceone.inventory.connector.virtual_machine_scale_set import VmScaleSetConnector
+from spaceone.inventory.connector.subscription import SubscriptionConnector
