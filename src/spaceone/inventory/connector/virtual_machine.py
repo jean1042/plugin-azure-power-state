b70b541a50087bcd34caedc8c153181a0b9aad14
jean1042 <jean1042@naver.com>
2021-03-18 15:55:49 +0900

    change list_vms() function to list_all()

--- src/spaceone/inventory/connector/virtual_machine.py
+++ src/spaceone/inventory/connector/virtual_machine.py
@@ -12,9 +12,14 @@ class VirtualMachineConnector(AzureConnector):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
+        self.set_connect(kwargs.get('secret_data'))
+    '''
     def list_vms(self, resource_group_name, **query):
         return list(self.compute_client.virtual_machines.list(resource_group_name=resource_group_name, **query))
+    '''
+
+    def list_vms(self):
+        return self.compute_client.virtual_machines.list_all()
 
     def get_vm(self, resource_group_name, vm_name):
         return self.compute_client.virtual_machines.get(resource_group_name, vm_name, expand='instanceView')
