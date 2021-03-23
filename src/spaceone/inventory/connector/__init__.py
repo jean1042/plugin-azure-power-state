b57a591de81287cc0b9b5bfc915b1675090cf00d
jean1042 <jean1042@naver.com>
2021-03-23 16:30:25 +0900

    Add monitor connector to connector init file

--- src/spaceone/inventory/connector/__init__.py
+++ src/spaceone/inventory/connector/__init__.py
@@ -3,3 +3,4 @@ from spaceone.inventory.connector.virtual_machine import VirtualMachineConnector
 from spaceone.inventory.connector.virtual_machine_scale_set import VmScaleSetConnector
 from spaceone.inventory.connector.subscription import SubscriptionConnector
 from spaceone.inventory.connector.sql import SqlConnector
+from spaceone.inventory.connector.monitor import MonitorConnector
